4192a716b3318075ed4337fc313559905b4ca5d0
AdamMudrak <mudrets2000@gmail.com>
2022-12-08 18:40:13 +0200

    Add multiplication

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,6 +4,7 @@ public class Main {
     static double number1;
     static double number2;
     static double sum;
+    static double times;
     public static void main(String[] args) {
         System.out.println("I actually used main");
 
@@ -12,10 +13,16 @@ public class Main {
         number2 = 2;
 
         System.out.println(number1 + " plus " + number2 + " equals " + addition(number1, number2));
+
+        System.out.println(number1 + " times " + number2 + " equals " + multiplication(number1, number2));
     }
 
     public static double addition(double number1, double number2) {
         sum = number1 + number2;
         return sum;
+    }
+    public static double multiplication(double number1, double number2) {
+        times = number1 * number2;
+        return times;
     }
 }
